5a93fab20803a09203aa40d7c1e36604be1ee1ad
adricosmicfox <adriiphone@gmail.com>
2022-09-22 15:30:08 +0200

    created project and text

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <MyFirstText />
+      </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import MyFirstText from './components/MyFirstText.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    MyFirstText,
+}
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MyFirstText.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="App"> 
+
+    <h1>La mia prima app con Vue CLI!  </h1>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+name: 'MyFirstText',
+}
+</script>
+
+<style>
+
+</style>
